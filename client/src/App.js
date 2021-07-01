13c9da4449e26191204ee50cd3e3e5f6b5ccd5bb
Neon Riot <sbrookshire99@gmail.com>
2021-07-01 15:35:20 -0500

    Began work on the dashboard view, almost finished with the base html and css, not ported to react yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
@@ -10,12 +10,19 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AddPark from "./pages/addPark";
+// import Dashboard from "./pages/Dashboard";
 const client = new ApolloClient({
   uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
 function App() {
+  const [token, setToken] = useState();
+
+  // if (!token) {
+  //   return <Dashboard />;
+  // }
+
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -25,7 +32,7 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/createUser" component={CreateUser} />
-            <Route path="/addPark" component={AddPark} />
+            <Route exact path="/addPark" component={AddPark} />
           </Switch>
           <Footer />
         </div>
