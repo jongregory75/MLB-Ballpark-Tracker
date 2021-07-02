a8614946f9b16509f80fc1a69bc353b876aab30e
Neon Riot <sbrookshire99@gmail.com>
2021-07-02 13:53:27 -0500

    Actually added user authentication, utils, and js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,44 +1,59 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-// const bcrypt = require('bcrypt')
+import { useMutation } from "@apollo/client";
+import { SIGN_IN } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error, data }] = useMutation(SIGN_IN);
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-  function handleSubmit(event) {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-  }
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <main className="form-signin text-center">
-      <form id="loginForm">
+      <form onSubmit={handleFormSubmit}>
         <img className="mb-4" src={logo} alt="Our logo" />
         <h1 className="h3 mb-3 fw-normal">Login Here</h1>
         <div className="form-floating" id="loginEmail">
           <input
             type="email"
-            value={email}
+            value={formState.email}
             className="form-control"
             id="floatingEmail"
             placeholder="name@example.com"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleChange}
           />
           <label htmlFor="floatingEmail">Email address</label>
         </div>
         <div className="form-floating" id="loginPassword">
           <input
             type="password"
-            value={password}
+            value={formState.password}
             className="form-control"
             id="floatingPassword"
             placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handleChange}
           />
           <label htmlFor="floatingPassword">Password</label>
         </div>
@@ -48,7 +63,11 @@ const Login = () => {
             <p>Remember Me</p>
           </label>
         </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
+        <button
+          className="w-100 btn btn-lg btn-primary"
+          type="submit"
+          disabled={!validateForm()}
+        >
           Login
         </button>
         <Link to="/createUser">
