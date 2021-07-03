eae10cc0330558c4b451b1595b31245f04097673
Jon Gregory <jongregory75@hotmail.com>
2021-07-03 11:08:31 -0600

    barInfo and insidePark seed, resolver, typedef

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const BallPark = require("./BallPark");
 const BarInfo = require("./BarInfo");
 const Profile = require("./Profile");
+const InsidePark = require("./InsidePark");
 
-module.exports = { BallPark, BarInfo, Profile };
+module.exports = { BallPark, BarInfo, Profile, InsidePark };
